71d67c2577cb0ccb7faa6fe8c957d6aed0f9157e
MilesStanley <74632615+MilesStanley@users.noreply.github.com>
2024-10-12 22:18:11 -0700

    fic

--- BuzzVault/src/main.jsx
+++ BuzzVault/src/main.jsx
@@ -5,30 +5,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import RootLayout from './routes/layout/RootLayout';
-import OnBoarding from './routes/onBoarding';
-<<<<<<< HEAD
-import CapsuleCollectionApp from './routes/CapsuleCollection/CapsuleCollectionApp';
 import ChatApp from './routes/Chat/ChatApp';
 
 const router = createBrowserRouter([
   {
     element: <ChatApp />,
     path: "/"
-=======
-import CountDown from './routes/countDown/CountDown';
-
-const router = createBrowserRouter([
-  {
-    element: <CountDown />,
-    path:"/"
->>>>>>> 9fbc312fbcc7ee54676acdfce29fdad121581e8f
-    // children: [
-    //   {
-    //     path: "/",
-    //     element: <OnBoarding />
-    //   }
-    // ]
   }
 ])
 
